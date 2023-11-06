6df8aa55faeef324525e688cd41d5d7b6cdbcf90
olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-11-06 15:26:54 +0100

    made changes to the user-document so that template duplication can be handled in the frontend

--- src/api/user-document/controllers/user-document.js
+++ src/api/user-document/controllers/user-document.js
@@ -4,196 +4,200 @@
  * user-document controller
  */
 
-module.exports = {
-  /**
-   * Create-new-use document
-   */
-  async createDocument(ctx) {
-    const { user } = ctx.state; //Get the logged-in user
-    const { templateId, company } = ctx.request.body;
+// module.exports = {
+//   /**
+//    * Create-new-use document
+//    */
+//   async createDocument(ctx) {
+//     const { user } = ctx.state; //Get the logged-in user
+//     const { templateId, company } = ctx.request.body;
 
-    // Find the template
-    // const template = await strapi.entityService.findOne(
-    //   "api::template.template",
-    //   templateId,
-    //   {
-    //     populate: {
-    //       coverLetter: {
-    //         populate: {
-    //           section: true,
-    //           previewImage: true,
-    //         },
-    //       },
-    //       resume: {
-    //         populate: {
-    //           sections: true,
-    //           previewImage: true,
-    //           customSection: true,
-    //         },
-    //       },
-    //     },
-    //   }
-    // );
+//     // Find the template
+//     // const template = await strapi.entityService.findOne(
+//     //   "api::template.template",
+//     //   templateId,
+//     //   {
+//     //     populate: {
+//     //       coverLetter: {
+//     //         populate: {
+//     //           section: true,
+//     //           previewImage: true,
+//     //         },
+//     //       },
+//     //       resume: {
+//     //         populate: {
+//     //           sections: true,
+//     //           previewImage: true,
+//     //           customSection: true,
+//     //         },
+//     //       },
+//     //     },
+//     //   }
+//     // );
 
-    const template = await strapi.entityService.findOne(
-      "api::template.template",
-      templateId,
-      {
-        populate: {
-          template: {
-            populate: {
-              coverLetter: {
-                populate: {
-                  sections: true,
-                  previewImage: true,
-                },
-              },
-              resume: {
-                populate: {
-                  sections: true,
-                  previewImage: true,
-                },
-              },
-            },
-          },
-        },
-      }
-    );
+//     const template = await strapi.entityService.findOne(
+//       "api::template.template",
+//       templateId,
+//       {
+//         populate: {
+//           template: {
+//             populate: {
+//               coverLetter: {
+//                 populate: {
+//                   sections: true,
+//                   previewImage: true,
+//                 },
+//               },
+//               resume: {
+//                 populate: {
+//                   sections: true,
+//                   previewImage: true,
+//                 },
+//               },
+//             },
+//           },
+//         },
+//       }
+//     );
 
-    console.log(template);
-    if (!template) {
-      return ctx.throw(404, "Template not found");
-    }
+//     console.log(template);
+//     if (!template) {
+//       return ctx.throw(404, "Template not found");
+//     }
 
-    // if the template is found, clone the template to the userdocument
-    const create = await strapi.entityService.create(
-      "api::user-document.user-document",
-      {
-        data: {
-          users_permissions_user: user.id,
-          title: company || "",
-          template: {
-            coverLetter: template.template.coverLetter,
-            resume: template.template.resume,
-          },
-        },
-      }
-    );
+//     // if the template is found, clone the template to the userdocument
+//     const create = await strapi.entityService.create(
+//       "api::user-document.user-document",
+//       {
+//         data: {
+//           users_permissions_user: user.id,
+//           title: company || "",
+//           template: {
+//             coverLetter: template.template.coverLetter,
+//             resume: template.template.resume,
+//           },
+//         },
+//       }
+//     );
 
-    console.log("This is the document", create);
+//     console.log("This is the document", create);
 
-    ctx.send({
-      status: true,
-      data: create,
-      message: "Document created successfully",
-    });
-  },
-  async getDocumentsByUser(ctx) {
-    const { user } = ctx.state;
+//     ctx.send({
+//       status: true,
+//       data: create,
+//       message: "Document created successfully",
+//     });
+//   },
+//   async getDocumentsByUser(ctx) {
+//     const { user } = ctx.state;
 
-    // get all user documents
-    const document = await strapi.entityService.findMany(
-      "api::user-document.user-document",
-      {
-        filters: {
-          users_permissions_user: user.id,
-        },
-        populate: {
-          template: {
-            populate: {
-              coverLetter: {
-                populate: {
-                  sections: true,
-                  previewImage: true,
-                },
-              },
-              resume: {
-                populate: {
-                  sections: true,
-                  previewImage: true,
-                  customSection: true,
-                },
-              },
-            },
-          },
-        },
-      }
-    );
-    return ctx.send({
-      status: true,
-      data: document,
-    });
-  },
+//     // get all user documents
+//     const document = await strapi.entityService.findMany(
+//       "api::user-document.user-document",
+//       {
+//         filters: {
+//           users_permissions_user: user.id,
+//         },
+//         populate: {
+//           template: {
+//             populate: {
+//               coverLetter: {
+//                 populate: {
+//                   sections: true,
+//                   previewImage: true,
+//                 },
+//               },
+//               resume: {
+//                 populate: {
+//                   sections: true,
+//                   previewImage: true,
+//                   customSection: true,
+//                 },
+//               },
+//             },
+//           },
+//         },
+//       }
+//     );
+//     return ctx.send({
+//       status: true,
+//       data: document,
+//     });
+//   },
 
-  async getSingleDocument(ctx) {
-    const { id } = ctx.params;
-    const { user } = ctx.state;
+//   async getSingleDocument(ctx) {
+//     const { id } = ctx.params;
+//     const { user } = ctx.state;
 
-    const findUserDoc = await strapi.db
-      .query("api::user-document.user-document")
-      .findOne({
-        where: {
-          users_permissions_user: user.id,
-          id,
-        },
-      });
+//     const findUserDoc = await strapi.db
+//       .query("api::user-document.user-document")
+//       .findOne({
+//         where: {:1337/api/template/1?populate=template.*
+//           users_permissions_user: user.id,
+//           id,
+//         },
+//       });
 
-    if (!findUserDoc) return ctx.throw(404, "document not found");
+//     if (!findUserDoc) return ctx.throw(404, "document not found");
 
-    const document = await strapi.entityService.findOne(
-      "api::user-document.user-document",
-      id,
-      {
-        filters: {
-          users_permissions_user: user.id,
-        },
-        populate: {
-          template: {
-            populate: {
-              coverLetter: {
-                populate: {
-                  sections: true,
-                  previewImage: true,
-                },
-              },
-              resume: {
-                populate: {
-                  sections: true,
-                  previewImage: true,
-                },
-              },
-            },
-          },
-        },
-      }
-    );
+//     const document = await strapi.entityService.findOne(
+//       "api::user-document.user-document",
+//       id,
+//       {
+//         filters: {
+//           users_permissions_user: user.id,
+//         },
+//         populate: {
+//           template: {
+//             populate: {
+//               coverLetter: {
+//                 populate: {
+//                   sections: true,
+//                   previewImage: true,
+//                 },
+//               },
+//               resume: {
+//                 populate: {
+//                   sections: true,
+//                   previewImage: true,
+//                 },
+//               },
+//             },
+//           },
+//         },
+//       }
+//     );
 
-    return ctx.send({
-      data: document,
-      status: true,
-    });
-  },
+//     return ctx.send({
+//       data: document,
+//       status: true,
+//     });
+//   },
 
-  async deleteDocument(ctx) {
-    const { id } = ctx.params;
-    const { user } = ctx.state;
+//   async deleteDocument(ctx) {
+//     const { id } = ctx.params;
+//     const { user } = ctx.state;
 
-    const findUserDoc = await strapi.db
-      .query("api::user-document.user-document")
-      .findOne({
-        where: {
-          users_permissions_user: user.id,
-          id,
-        },
-      });
+//     const findUserDoc = await strapi.db
+//       .query("api::user-document.user-document")
+//       .findOne({
+//         where: {
+//           users_permissions_user: user.id,
+//           id,
+//         },
+//       });
 
-    if (!findUserDoc) return ctx.throw(404, "document not found");
+//     if (!findUserDoc) return ctx.throw(404, "document not found");
 
-    await strapi.entityService.delete("api::user-document.user-document", id);
+//     await strapi.entityService.delete("api::user-document.user-document", id);
 
-    ctx.send({
-      status: true,
-      message: "Document deleted successfully",
-    });
-  },
-};
+//     ctx.send({
+//       status: true,
+//       message: "Document deleted successfully",
+//     });
+//   },
+// };
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::user-document.user-document");
--- src/api/user-document/routes/user-document.js
+++ src/api/user-document/routes/user-document.js
@@ -1,40 +1,44 @@
-module.exports = {
-  routes: [
-    {
-      method: "POST",
-      path: "/create-document",
-      handler: "user-document.createDocument",
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: "GET",
-      path: "/documents",
-      handler: "user-document.getDocumentsByUser",
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: "GET",
-      path: "/document/:id",
-      handler: "user-document.getSingleDocument",
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: "DELETE",
-      path: "/document/:id",
-      handler: "user-document.deleteDocument",
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
+// module.exports = {
+//   routes: [
+//     {
+//       method: "POST",
+//       path: "/create-document",
+//       handler: "user-document.createDocument",
+//       config: {
+//         policies: [],
+//         middlewares: [],
+//       },
+//     },
+//     {
+//       method: "GET",
+//       path: "/documents",
+//       handler: "user-document.getDocumentsByUser",
+//       config: {
+//         policies: [],
+//         middlewares: [],
+//       },
+//     },
+//     {
+//       method: "GET",
+//       path: "/document/:id",
+//       handler: "user-document.getSingleDocument",
+//       config: {
+//         policies: [],
+//         middlewares: [],
+//       },
+//     },
+//     {
+//       method: "DELETE",
+//       path: "/document/:id",
+//       handler: "user-document.deleteDocument",
+//       config: {
+//         policies: [],
+//         middlewares: [],
+//       },
+//     },
+//   ],
+// };
+
+const { createCoreRouter } = require("@strapi/strapi").factories;
+
+module.exports = createCoreRouter("api::user-document.user-document");
